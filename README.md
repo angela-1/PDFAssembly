75667200879d822f11bb48873087deec8119dc04
angela-1 <angela-1@users.noreply.github.com>
2020-03-25 09:08:56 +0800

    add readme.

---
+++ README.md
@@ -0,0 +1,5 @@
+# PDFAssembly 
+
+readme
+
+生成 html 后放到 resources 文件夹
--- src/main/java/com/angela/ui/view/Nav.java
+++ src/main/java/com/angela/ui/view/Nav.java
@@ -2,17 +2,26 @@ package com.angela.ui.view;
 
 import com.angela.Utils;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class Nav extends VBox {
     private final SimpleStringProperty selected;
 
     public Nav() {
-        setPadding(new Insets(16, 0,8,0));
+        setPadding(new Insets(16, 0, 0, 0));
         setStyle("-fx-background-color: #e5e5e5;");
         setPrefWidth(180);
 
@@ -43,9 +52,36 @@ public class Nav extends VBox {
         });
 
         VBox white = new VBox();
-        white.getChildren().add(new Label("cnie"));
+
+        HBox links = new HBox();
+        Button help = new Button();
+        help.setGraphic(Utils.getImageView("/image/help.png", 16));
+        help.getStyleClass().add("link-button");
+        help.setOnAction(actionEvent -> {
+            try {
+                String readme = getClass().getResource("/README.html").toExternalForm();
+                Desktop.getDesktop().browse(new URI(readme));
+            } catch (IOException | URISyntaxException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Button github = new Button();
+        github.setGraphic(Utils.getImageView("/image/github.png", 16));
+        github.getStyleClass().add("link-button");
+        github.setOnAction(actionEvent -> {
+            try {
+                Desktop.getDesktop().browse(new URI("https://github.com/angela-1/PDFAssembly"));
+            } catch (IOException | URISyntaxException e) {
+                e.printStackTrace();
+            }
+        });
+
+        links.getChildren().addAll(help, github);
+        white.getChildren().add(links);
+
         getChildren().addAll(navList, white);
-        VBox.setVgrow(white, Priority.ALWAYS);
+        VBox.setVgrow(navList, Priority.ALWAYS);
     }
 
     public String getSelected() {
